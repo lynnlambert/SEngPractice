35aba429dc546bf77dd7e9d02211e34036d7eb7c
quachminhphu <phu.quach.13@cnu.edu>
2015-10-08 19:11:42 -0400

    getFirst implement

--- Name.java
+++ Name.java
@@ -37,7 +37,7 @@ public class Name {
 
     public String getFirst() {
         /* return Name's first name */
-        return first;
+        return this.first;
     }
 
     public String getMiddle() {
