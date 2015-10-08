d308f166629353da310c510a382f87735ef89606
kmcdonald1494 <kaylamcdonald@Kaylas-MacBook-Pro.local>
2015-10-08 18:52:16 -0400

    edit parent1 surname

--- Name.java
+++ Name.java
@@ -52,7 +52,7 @@ public class Name {
 
     public String getSurnameParent1() {
         /* return parent1's last name */
-        return "none";
+        return surnameParent1;
     }
 
     public String getSurnameParent2() {
